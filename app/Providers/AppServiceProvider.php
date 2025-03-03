609d41aabf6f43c1e1f7e2b941e08b014da72087
MoatasemGamal <moatasemco@gmail.com>
2025-03-03 18:04:36 +0200

    feat: install laravel elqouent to deal with wordpress site database
    - auto configure elqouent when boot from config.json or wp-config if exists

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Valuestore\Valuestore;
 use function Termwind\render;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Valuestore $config): void
     {
         if ($_SERVER['argv'][1] !== 'setup' && !Storage::exists('config.json')) {
             render(<<<'HTML'
@@ -25,6 +28,23 @@ class AppServiceProvider extends ServiceProvider
             HTML);
             exit();
         }
+
+        DB::purge('default');
+        Config::set('database.connections.default.driver', $config->get('db'));
+        if (File::exists(getcwd() . '/wp-config.php')) {
+            include getcwd() . '/wp-config.php';
+            Config::set('database.connections.default.username', DB_USER);
+            Config::set('database.connections.default.password', DB_PASSWORD);
+            Config::set('database.connections.default.database', DB_NAME);
+            Config::set('database.connections.default.host', DB_HOST);
+            Config::set('database.connections.default.charset', DB_CHARSET);
+            Config::set('database.connections.default.collation', DB_COLLATE);
+            Config::set('database.connections.default.prefix', $table_prefix);
+        } else {
+            Config::set('database.connections.default.username', $config->get('dbuser'));
+            Config::set('database.connections.default.password', $config->get('dbpass'));
+        }
+        DB::reconnect('default');
     }
 
     /**
