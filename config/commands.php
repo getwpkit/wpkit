abea84bab0e9cbb95941e9e5af5055be34f09d46
MoatasemGamal <moatasemco@gmail.com>
2025-03-07 02:52:22 +0200

    hide database commands if app in production

--- config/commands.php
+++ config/commands.php
@@ -54,16 +54,38 @@ return [
     |
     */
 
-    'hidden' => [
-        NunoMaduro\LaravelConsoleSummary\SummaryCommand::class,
-        Symfony\Component\Console\Command\DumpCompletionCommand::class,
-        Symfony\Component\Console\Command\HelpCommand::class,
-        Illuminate\Console\Scheduling\ScheduleRunCommand::class,
-        Illuminate\Console\Scheduling\ScheduleListCommand::class,
-        Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
-        Illuminate\Foundation\Console\VendorPublishCommand::class,
-        LaravelZero\Framework\Commands\StubPublishCommand::class,
-    ],
+    'hidden' =>
+        config('app.env') === 'production' ? [
+            NunoMaduro\LaravelConsoleSummary\SummaryCommand::class,
+            Symfony\Component\Console\Command\DumpCompletionCommand::class,
+            Symfony\Component\Console\Command\HelpCommand::class,
+            Illuminate\Console\Scheduling\ScheduleRunCommand::class,
+            Illuminate\Console\Scheduling\ScheduleListCommand::class,
+            Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
+            Illuminate\Foundation\Console\VendorPublishCommand::class,
+            LaravelZero\Framework\Commands\StubPublishCommand::class,
+
+            \Illuminate\Database\Console\WipeCommand::class,
+            \Illuminate\Database\Console\Migrations\MigrateMakeCommand::class,
+            \Illuminate\Database\Console\Migrations\FreshCommand::class,
+            \Illuminate\Database\Console\Migrations\InstallCommand::class,
+            \Illuminate\Database\Console\Migrations\MigrateCommand::class,
+            \Illuminate\Database\Console\Migrations\RefreshCommand::class,
+            \Illuminate\Database\Console\Migrations\ResetCommand::class,
+            \Illuminate\Database\Console\Migrations\RollbackCommand::class,
+            \Illuminate\Database\Console\Migrations\StatusCommand::class,
+            \Illuminate\Database\Console\Seeds\SeedCommand::class,
+        ] :
+        [
+            NunoMaduro\LaravelConsoleSummary\SummaryCommand::class,
+            Symfony\Component\Console\Command\DumpCompletionCommand::class,
+            Symfony\Component\Console\Command\HelpCommand::class,
+            Illuminate\Console\Scheduling\ScheduleRunCommand::class,
+            Illuminate\Console\Scheduling\ScheduleListCommand::class,
+            Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
+            Illuminate\Foundation\Console\VendorPublishCommand::class,
+            LaravelZero\Framework\Commands\StubPublishCommand::class,
+        ],
 
     /*
     |--------------------------------------------------------------------------
