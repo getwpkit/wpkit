c8bb6e79609dbf6ee31df56272e879ca8dec3afb
MoatasemGamal <moatasemco@gmail.com>
2025-03-08 17:26:35 +0200

    feat: add path argument
    - to enable erase outside the wp directory and delete the wp directory itself

--- app/Commands/EraseCommand.php
+++ app/Commands/EraseCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Commands;
 
+use Config;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
@@ -14,7 +15,7 @@ class EraseCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'erase';
+    protected $signature = 'erase {path=.}';
 
     /**
      * The console command description.
@@ -28,13 +29,16 @@ class EraseCommand extends Command
      */
     public function handle()
     {
-        if (!File::exists(getcwd() . '/wp-config.php')) {
+        $path = realpath($this->argument('path'));
+
+        if (!File::exists($path . '/wp-config.php')) {
             $this->fail('No wordpress files detected');
             exit(1);
         }
 
 
-        $this->task('Database Drop', function () {
+        $this->task('Database Drop', function () use ($path) {
+            $this->restartDatabase($path);
             $dbname = config('database.connections.default.database');
             try {
                 return DB::statement("DROP DATABASE IF EXISTS `$dbname`");
@@ -42,8 +46,9 @@ class EraseCommand extends Command
                 $this->fail('Error dropping database: ' . $e->getMessage());
             }
         });
-        $this->task('Removing WP files', function () {
-            $this->deleteAllInDirectory(getcwd());
+        $this->task('Removing WP files', function () use ($path) {
+            $this->deleteAllInDirectory($path);
+            rmdir($path);
         });
     }
 
@@ -73,6 +78,35 @@ class EraseCommand extends Command
                 if (!$result)
                     $success = $result;
             }
+        }
+    }
+
+    private function restartDatabase($path)
+    {
+        if (File::exists($path . '/wp-config.php')) {
+            DB::purge('default');
+            $file = $path . '/wp-config.php';
+            $file_contents = file_get_contents($file);
+            // Regex to find constant definitions
+            preg_match_all('/define\s*\(\s*["\']([A-Za-z0-9_]+)["\']\s*,\s*["\']([^"\']+)["\']\s*\)/', $file_contents, $matches);
+            // Define the constants dynamically
+            foreach ($matches[1] as $index => $constantName) {
+                $constantValue = $matches[2][$index];
+                define($constantName, $constantValue);
+            }
+            // Regex pattern to match $table_prefix definition
+            $pattern = "/table_prefix\s*=\s*'(.*?)';/";
+            preg_match($pattern, $file_contents, $matches);
+            $table_prefix = $matches[1];
+
+            Config::set('database.connections.default.username', DB_USER);
+            Config::set('database.connections.default.password', DB_PASSWORD);
+            Config::set('database.connections.default.database', DB_NAME);
+            Config::set('database.connections.default.host', DB_HOST);
+            Config::set('database.connections.default.charset', DB_CHARSET);
+            Config::set('database.connections.default.collation', defined('DB_COLLATE') ? DB_COLLATE : null);
+            Config::set('database.connections.default.prefix', $table_prefix);
+            DB::reconnect('default');
         }
     }
 }
